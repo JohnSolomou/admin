0c278f8e8561c0b3640d0b4757296cbcb0ddd1ca
John Solomou <solomoufam@yahoo.com>
2023-05-30 14:27:42 -0400

    fixed some spelling errors

--- src/components/featruedinfo/FeaturedInfo.jsx
+++ src/components/featruedinfo/FeaturedInfo.jsx
@@ -5,14 +5,14 @@ export default function FeaturedInfo() {
   return (
     <div className="featured">
       <div className="featuredItem">
-        <span className="featuredTitle">Revanue</span>
+        <span className="featuredTitle">Revenue</span>
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">$4,415</span>
           <span className="featuredMoneyRate">
             -11.4 <ArrowDownward className="featuredIcon negative" />
           </span>
         </div>
-        <span className="featuredsub">compaired to last month</span>
+        <span className="featuredsub">compared to last month</span>
       </div>
       <div className="featuredItem">
         <span className="featuredTitle">sales</span>
@@ -22,7 +22,7 @@ export default function FeaturedInfo() {
             -11.4 <ArrowDownward className="featuredIcon negative" />
           </span>
         </div>
-        <span className="featuredsub">compaired to last month</span>
+        <span className="featuredsub">compared to last month</span>
       </div>
       <div className="featuredItem">
         <span className="featuredTitle">cost</span>
@@ -32,7 +32,7 @@ export default function FeaturedInfo() {
             +2.4 <ArrowUpward className="featuredIcon " />
           </span>
         </div>
-        <span className="featuredsub">compaired to last month</span>
+        <span className="featuredsub">compared to last month</span>
       </div>
     </div>
   );
